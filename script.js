62e327a727e80c90939835dcc07808c15dc583f6
Jan_Jagoda <jan.jagoda@outlook.com>
2023-08-19 18:07:51 +0200

    Completed Exercise 2

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ getSquaredNumbers([6, 7, 8, 9, 10]); // [36, 49, 64, 81, 100]
 const numbers = [1, 2, 3];
 getSquaredNumbers(numbers); // [1, 4, 9]
 console.log(numbers); // [1, 2, 3]
+
+// Exercise 2
+
+function getReversedString(word) {
+    let reversedWordsArray = [];
+    let letter = word.split('');
+    for (let i=0; i < word.length; ++i ) {
+        reversedWordsArray.unshift(letter[i]);
+    }
+    reversedString = reversedWordsArray.join("")
+    return console.log(reversedString);
+}
+
+getReversedString('Hello!'); // '!olleH'
+getReversedString('Arrays'); // 'syarrA'
